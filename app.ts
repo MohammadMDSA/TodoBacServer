087fc33bcbdde240e77d979c32aa96976761993b
Mohammad MDSA <samimd.77@gmail.com>
2018-06-25 01:53:32 +0430

    Added server config

--- app.ts
+++ app.ts
@@ -1,6 +1,22 @@
 import Server from "bacjs/out/server/server";
 import Config from "bacjs/out/server/config";
 
-let config: 
+let config: Config = {
+	routers: [],
 
-let server = new Server();
+	auth: {
+		secret: "My secret",
+		session: {
+			limited: false
+		}
+	},
+
+	mongo: {
+		connection: "mongodb://localhost/test"
+	},
+	port: 9000
+};
+
+let server = new Server(config);
+
+server.start()
